07867a6da374c3959bde78ea7a1779b101595a72
Mino1910 <jasmin.sailer@web.de>
2025-06-19 11:41:07 +0200

    marker

--- marker.py
+++ marker.py
@@ -103,7 +103,7 @@ def process_xml_content(xml_content):
     
     innen_elem = root.findall(".//lido:subjectSet/lido:subject[lido:type='keyword']/lido:subjectConcept/lido:term", ns)
     innen = [elem.text.strip() for elem in innen_elem if elem is not None and elem.text]
-    if "Bibliothek" in innen or "Innenraum" in innen:
+    if "Bibliothek" in innen or "Innenraum" in innen or "Gruppe" in innen:
         return None
 
     id_elem = root.find(".//lido:lidoRecID", ns)
